38be678f68337ba94950bb2f768cb8ae22f49540
leowu0329 <ryowuandjanet@gmail.com>
2025-06-07 23:31:57 +0000

    新增個人資料修改

--- users/admin.py
+++ users/admin.py
@@ -1,12 +1,41 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
 from .forms import CustomUserCreationForm, CustomUserChangeForm
-from .models import CustomUser
+from .models import CustomUser, Profile
+
+# 內聯管理 Profile
+class ProfileInline(admin.StackedInline):
+    model = Profile
+    can_delete = False
+    verbose_name_plural = 'Profile'
+    fk_name = 'user'
+    
+    def get_queryset(self, request):
+        qs = super().get_queryset(request)
+        return qs.select_related('user')
 
 class CustomUserAdmin(UserAdmin):
-	add_form = CustomUserCreationForm
-	form = CustomUserChangeForm
-	list_display = ['email', 'username', 'age']
-	model = CustomUser
+    add_form = CustomUserCreationForm
+    form = CustomUserChangeForm
+    list_display = ['email', 'username', 'age']
+    model = CustomUser
+    inlines = [ProfileInline]  # 添加內聯
+    
+    # 重寫get_inline_instances方法，確保在添加使用者時不顯示內聯
+    def get_inline_instances(self, request, obj=None):
+        if not obj:
+            return []
+        try:
+            if not obj.profile:
+                Profile.objects.create(user=obj)
+        except Profile.DoesNotExist:
+            Profile.objects.create(user=obj)
+        return super().get_inline_instances(request, obj)
 
 admin.site.register(CustomUser, CustomUserAdmin)
+
+# 也可以單獨註冊Profile模型
+@admin.register(Profile)
+class ProfileAdmin(admin.ModelAdmin):
+    list_display = ['user', 'role', 'work_area']
+    search_fields = ['user__username', 'user__email']
--- users/views.py
+++ users/views.py
@@ -4,11 +4,15 @@ from .forms import CustomUserCreationForm
 from django.contrib.auth.views import PasswordResetConfirmView
 from django.urls import reverse_lazy
 from django.contrib.auth.views import LogoutView
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.views.generic import UpdateView
+from .models import Profile
+from .forms import ProfileForm
 
 class SignUp(generic.CreateView):
-    form_class = CustomUserCreationForm
-    success_url = reverse_lazy('login')
-    template_name = 'signup.html'
+	form_class = CustomUserCreationForm
+	success_url = reverse_lazy('login')
+	template_name = 'signup.html'
 
 class CustomPasswordResetConfirmView(PasswordResetConfirmView):
     template_name = 'registration/password_reset_confirm.html'
@@ -16,3 +20,19 @@ class CustomPasswordResetConfirmView(PasswordResetConfirmView):
 
 class CustomLogoutView(LogoutView):
     next_page = reverse_lazy('login')  # 設定登出後跳轉到登入頁面
+
+class ProfileView(LoginRequiredMixin, UpdateView):
+    model = Profile
+    form_class = ProfileForm
+    template_name = 'registration/profile.html'
+    success_url = reverse_lazy('profile')
+    
+    def get_object(self, queryset=None):
+        # Get or create profile for the current user
+        profile, created = Profile.objects.get_or_create(user=self.request.user)
+        return profile
+    
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['user'] = self.request.user
+        return context
